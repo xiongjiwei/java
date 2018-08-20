704773bb8591d59fad272d0d62f6e30da68be466
xiong jiwei <jwxiong@thoughtworks.com>
2018-08-20 21:23:25 +0800

    [jwxiong] finish inheritance test

--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/PersonForEquals.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/PersonForEquals.java
@@ -36,7 +36,11 @@ public class PersonForEquals {
     public boolean equals(Object obj) {
         // TODO: please modify the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        if(!(obj instanceof PersonForEquals)){
+            return false;
+        }
+        return this.name.equals(((PersonForEquals) obj).getName()) && this.yearOfBirth == ((PersonForEquals) obj).getYearOfBirth();
+
         // --end-->
     }
 
@@ -44,7 +48,7 @@ public class PersonForEquals {
     public int hashCode() {
         // TODO: please modify the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        return (this.name + this.yearOfBirth).hashCode();
         // --end-->
     }
 }
