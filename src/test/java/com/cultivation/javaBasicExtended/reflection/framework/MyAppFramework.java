e69a21a5dc2351d52ba89ec3eb5c9b785951f838
xiong jiwei <jwxiong@thoughtworks.com>
2018-08-23 18:46:35 +0800

    [jwxiong] finish work

--- src/test/java/com/cultivation/javaBasicExtended/reflection/framework/MyAppFramework.java
+++ src/test/java/com/cultivation/javaBasicExtended/reflection/framework/MyAppFramework.java
@@ -1,21 +1,74 @@
 package com.cultivation.javaBasicExtended.reflection.framework;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("WeakerAccess")
 public class MyAppFramework {
+
+    private Map<String, Class> controllerMap = new HashMap<>();
+
     public void registerController(Class controllerClazz) {
         // TODO: Please implement the method
         // <--start
-        throw new NotImplementedException();
+        if (controllerMap.containsKey(controllerClazz.getName())) {
+            throw new IllegalArgumentException();
+        }
+
+        controllerMap.put(controllerClazz.getName(), controllerClazz);
         // --end-->
     }
 
-    public Response getResponse(String requestController, String requestMethod) {
+    public Response getResponse(String requestController, String requestMethod) throws InstantiationException {
         // TODO: Please implement the method
         // <--start
-        throw new NotImplementedException();
+        Class<?> controller = getControllerClass(requestController);
+        if (controller != null) {
+            try {
+                Method method = controller.getDeclaredMethod(requestMethod);
+                Object result = method.invoke(controller.newInstance());
+                return getResponse(result);
+            } catch (NoSuchMethodException e) {
+                return getResponseWhenNoSuchMethod(requestMethod, controller);
+            } catch (InvocationTargetException e) {
+                return new Response(500);
+            } catch (IllegalAccessException e) {
+                return new Response(403);
+            }
+        }
+
+        return new Response(404);
         // --end-->
+    }
+
+    private Response getResponseWhenNoSuchMethod(String requestMethod, Class<?> controller) {
+        Method[] methods = controller.getMethods();
+        for (Method method :
+                methods) {
+            if (method.getName().equals(requestMethod)) {
+                return new Response(503);
+            }
+        }
+
+        return new Response(404);
+    }
+
+    private Response getResponse(Object result) {
+        if (result instanceof Response){
+            return (Response) result;
+        }
+
+        return new Response(503);
+    }
+
+    private Class<?> getControllerClass(String requestController) {
+        if (controllerMap.containsKey(requestController)) {
+            return controllerMap.get(requestController);
+        }
+
+        return null;
     }
 
     // TODO: You can add additional methods here if you want
--- src/test/java/com/cultivation/javaBasicExtended/reflection/test/MyAppFrameworkTest.java
+++ src/test/java/com/cultivation/javaBasicExtended/reflection/test/MyAppFrameworkTest.java
@@ -4,6 +4,8 @@ import com.cultivation.javaBasicExtended.reflection.framework.MyAppFramework;
 import com.cultivation.javaBasicExtended.reflection.framework.Response;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -17,7 +19,7 @@ class MyAppFrameworkTest {
     }
 
     @Test
-    void should_invoke_request() {
+    void should_invoke_request() throws IllegalAccessException, InvocationTargetException, InstantiationException {
         MyAppFramework app = new MyAppFramework();
         app.registerController(StandardController.class);
         Response response = app.getResponse(
@@ -28,7 +30,7 @@ class MyAppFrameworkTest {
     }
 
     @Test
-    void should_get_404_if_no_action_can_be_found() {
+    void should_get_404_if_no_action_can_be_found() throws IllegalAccessException, InvocationTargetException, InstantiationException {
         MyAppFramework app = new MyAppFramework();
         app.registerController(StandardController.class);
         Response response = app.getResponse(
@@ -40,7 +42,7 @@ class MyAppFrameworkTest {
     // Additional Test
 
     @Test
-    void should_get_404_if_no_controller_is_registered() {
+    void should_get_404_if_no_controller_is_registered() throws IllegalAccessException, InvocationTargetException, InstantiationException {
         MyAppFramework app = new MyAppFramework();
         Response response = app.getResponse(
             "com.cultivation.javaBasicExtended.reflection.test.StandardController", "doSomething");
@@ -49,7 +51,7 @@ class MyAppFrameworkTest {
     }
 
     @Test
-    void should_get_404_if_requested_controller_does_not_exist() {
+    void should_get_404_if_requested_controller_does_not_exist() throws IllegalAccessException, InvocationTargetException, InstantiationException {
         MyAppFramework app = new MyAppFramework();
         app.registerController(AnotherStandardController.class);
         Response response = app.getResponse(
@@ -59,7 +61,7 @@ class MyAppFrameworkTest {
     }
 
     @Test
-    void should_get_403_if_the_request_method_is_not_public() {
+    void should_get_403_if_the_request_method_is_not_public() throws IllegalAccessException, InvocationTargetException, InstantiationException {
         MyAppFramework app = new MyAppFramework();
         app.registerController(StandardController.class);
         Response response = app.getResponse(
@@ -69,7 +71,7 @@ class MyAppFrameworkTest {
     }
 
     @Test
-    void should_create_new_controller_instance_every_time() {
+    void should_create_new_controller_instance_every_time() throws IllegalAccessException, InvocationTargetException, InstantiationException {
         MyAppFramework app = new MyAppFramework();
         app.registerController(StatefulController.class);
 
@@ -83,7 +85,7 @@ class MyAppFrameworkTest {
     }
 
     @Test
-    void should_get_500_if_unhandled_exception_occurred_during_invocation() {
+    void should_get_500_if_unhandled_exception_occurred_during_invocation() throws IllegalAccessException, InvocationTargetException, InstantiationException {
         MyAppFramework app = new MyAppFramework();
         app.registerController(UnhandledExceptionController.class);
 
@@ -93,7 +95,7 @@ class MyAppFrameworkTest {
     }
 
     @Test
-    void should_get_503_if_action_return_type_is_not_response() {
+    void should_get_503_if_action_return_type_is_not_response() throws IllegalAccessException, InvocationTargetException, InstantiationException {
         MyAppFramework app = new MyAppFramework();
         app.registerController(BadActionController.class);
 
@@ -103,7 +105,7 @@ class MyAppFrameworkTest {
     }
 
     @Test
-    void should_get_503_if_action_contains_parameter() {
+    void should_get_503_if_action_contains_parameter() throws IllegalAccessException, InvocationTargetException, InstantiationException {
         MyAppFramework app = new MyAppFramework();
         app.registerController(BadActionController.class);
 
